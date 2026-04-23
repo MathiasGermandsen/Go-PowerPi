802ddb800ab60fb0ca7df3949869ba8966b08cfc
Mathias <mathias.germandsen@bestseller.com>
2026-04-23 09:04:35 +0200

    I'm just a grunt. Don't blame me for this awful PoS.

--- apis/handlers.go
+++ apis/handlers.go
@@ -11,14 +11,15 @@ import (
 
 func GetPowerTable(w http.ResponseWriter, r *http.Request) {
 	userID := r.URL.Query().Get("userId")
-	if userID == "" {
-		http.Error(w, "missing userId", http.StatusBadRequest)
-		return
-	}
 
 	var rows []database.PowerTable
 
-	if result := database.DB.Where("user_id = ?", userID).Find(&rows); result.Error != nil {
+	query := database.DB
+	if userID != "" {
+		query = query.Where("user_id = ?", userID)
+	}
+
+	if result := query.Find(&rows); result.Error != nil {
 		http.Error(w, result.Error.Error(), http.StatusInternalServerError)
 		return
 	}
@@ -52,13 +53,4 @@ func CreatePowerTable(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(req)
-}
-
-func TruncatePowerTable(w http.ResponseWriter, r *http.Request) {
-	if result := database.DB.Exec("TRUNCATE TABLE power_tables RESTART IDENTITY CASCADE"); result.Error != nil {
-		http.Error(w, result.Error.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	w.WriteHeader(http.StatusNoContent)
 }
